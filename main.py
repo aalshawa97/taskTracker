3eba6f2fdc2683bce68688f7b21d297a7ce5e6f6
abdullah97 <abdullahalshawa@gmail.com>
2023-07-06 05:40:58 -0700

    Boilerplate

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+#Abdullah Mutaz ALshawa
+#7/6/23
+#Task tracker
+from tkinter import *
+from tkinter import ttk
+
+#
+#label = tk.Label(root, text="Hello World!, font=('Arial', 18)
 class Task:
     def __init__(self, title, description, due_date, status="To Do"):
         self.title = title
@@ -59,7 +67,27 @@ if __name__ == '__main__':
     tasks = []  # Initialize an empty list to store tasks
     task = create_task("Coding", "Learn Cloud Computing", "7/3")
     tasks.append(task)  # Add the created task to the list
+    task = create_task("MMA", "Learn Sambo", "7/4")
+    tasks.append(task)  # Add the created task to the list
     print_tasks(tasks)
     task_index = int(input("Enter the index of the task you want to update: "))
     update_task(tasks, task_index)
     print_tasks(tasks)
+    root = Tk()
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    print_tasks(tasks)
+
+    for index, task in enumerate(tasks, start=1):
+        try:
+            print(f"Task {index}:")
+            print_task_details(task)
+            ttk.Label(frm, text=task.title).grid(column=0, row=index)
+            ttk.Label.pack(padx=200, pady=20)
+            myentry = ttk.Entry(root)
+            #   myentry.pack()
+        except Exception as e:
+            print(f"Error occurred while printing Task {index}: {e}")
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+    root.mainloop()
+    #root.mainloop(tasks)
